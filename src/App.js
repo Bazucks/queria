a0b368ddefa02acd89c64aa772b25fd7626d758c
blazed <blazerdork@gmail.com>
2022-10-23 22:01:50 +0300

    patch 0.97

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import Support from "./components/Support";
 import SExpertise from "./components/ServiceComps/SExpertise";
 import Trustproof from "./components/Trustproof";
 import ReactGA from "react-ga";
-const TRACKING_ID = "UA-243068835-1"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 
 function App() {
   useEffect(()=> {
-    ReactGA.pageview(window.location.pathname)
+    ReactGA.initialize("UA-243068835-1");
+
+    ReactGA.pageview('/')
   }, [])
   return (
     <div className="overflow-x-hidden">
